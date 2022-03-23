9c1a5c9122c0341795cc0b7040d2f786c6a9a781
jsoares07 <jfernandosoares7@gmail.com>
2022-03-23 20:07:26 +0000

    Added the feeling confident part

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon", "internet", "sergio"];
-let com = [".com", ".net", ".us", ".es", ".ie", ".pt", ".io"];
+let com = [".com", ".net", ".us", ".es", ".io", ".pt"];
 
 // const dmgenerator = [];
 
@@ -20,7 +20,18 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < com.length; l++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + com[l]);
+        let extWithoutTheDot = com[l].slice(1);
+
+        if (noun[k].slice(-extWithoutTheDot.length) == extWithoutTheDot) {
+          console.log(
+            pronoun[i] +
+              adj[j] +
+              noun[k].slice(0, noun[k].length - extWithoutTheDot.length) +
+              com[l]
+          );
+        } else {
+          console.log(pronoun[i] + adj[j] + noun[k] + com[l]);
+        }
       }
     }
   }
